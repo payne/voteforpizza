6c406bc52cc57ccd98c6a400d4b5d1cd23271588
Mat Moore <mwehtam@gmail.com>
2015-05-02 16:35:12 +0100

    Add new election form

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import play.api._
+import play.api.data.validation.{Valid, Invalid, Constraint}
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
@@ -11,6 +12,7 @@ import play.api.i18n.Lang
 object Application extends Controller {
   val candidates = List("foo", "bar", "baz")
   case class Ballot(preferences: List[Int])
+  case class Election(candidates: List[String], name: String, description: String)
 
   def validateRankingIsComplete(preferences: List[Int]): Boolean = {
     val received = preferences.sorted.toList
@@ -28,6 +30,19 @@ object Application extends Controller {
       fields => validateRankingIsComplete(fields.preferences)
     )
   )
+
+  val electionForm: Form[Election] = {
+    val filterNonEmpty: List[String] => List[String] = _.filter(_ != "")
+    Form(
+      mapping(
+        "candidates" -> list(text).transform(filterNonEmpty, identity[List[String]]).verifying(
+          "Two or more items required", _.size >= 2
+        ),
+        "name" -> nonEmptyText,
+        "description" -> text
+      )(Election.apply)(Election.unapply)
+    )
+  }
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
@@ -57,5 +72,22 @@ object Application extends Controller {
   }
 
   def count(election: String) = TODO
+
+  def showNewElectionForm() = Action {
+    Ok(views.html.electionForm(electionForm))
+  }
+
+  def createElection() = Action {implicit request =>
+     electionForm.bindFromRequest.fold(
+      formWithErrors => {
+        Logger.debug("Oh no")
+        Logger.debug(formWithErrors.errorsAsJson(Lang("en")).toString)
+        BadRequest(views.html.electionForm(formWithErrors))
+      },
+      value => {
+        Logger.debug("Yay")
+        Redirect(routes.Application.show(value.name)).flashing("success" -> "Created new choice.")
+      }
+    )}
 
 }
